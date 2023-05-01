3dfc6006964fe5c405d50146f435c9057b2593d7
Jiwon Kim <luvgy@naver.com>
2023-05-02 02:38:14 +0900

    내 주석 수정

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App /> //앱이야
+  <App /> //앱이야 //수정이 중복되면 어떻게 될까?
   // </React.StrictMode>
 );
 
