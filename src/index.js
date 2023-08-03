bbd9de0e2f122fb2640e6f638e1ac08a22118e4f
jiwonkim <luvgy@naver.com>
2023-08-03 11:24:43 +0900

    chapter.14 / chapter.15 +styled-componets

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import ProfileCard from "./chapter.13/ProfileCard";
+import Blocks from "./chapter_15/Blocks";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
-  <ProfileCard />
+  <Blocks />
   //</React.StrictMode>
 );
 
